431cae5071a8236c5f31883d3d6e4ed2d49a894e
adilzubari <adilzubari852@gmail.com>
2021-09-17 23:07:33 +0500

    committing for heroku

--- app.js
+++ app.js
@@ -382,8 +382,15 @@ app.post("/location/push", (req, res) => {
 // exports.app = functions.https.onRequest(app);
 
 // Listener
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Listening at http://localhost:${port}`);
+// });
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
 
 // "dependencies": {
